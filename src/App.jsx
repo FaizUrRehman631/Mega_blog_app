505c7b927783da26a538fe052d770bfd5b88e10e
FaizUrRehman <shadowwalker3600@gmail.com>
2025-01-26 20:15:17 +0500

    Megablog_app

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+  console.log(import.meta.env.VITE_APPWRITE_URL)
+
+  return (
+    <>
+      <h1 className='bg-gray-400 text-white text-center '>A blog app with appwrite</h1>
+    </>
+  )
+}
+
+export default App;
